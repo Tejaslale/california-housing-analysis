8bb9c208643a1d7d4f17d4064b2511b7305bfc47
Tejas Lale <tejaslale29@gmail.com>
2024-08-24 12:09:17 +0530

    Update all files with recent changes

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import pickle
 from flask import Flask,request,app,jsonift,url_for,render_template
+import numpy as np
+import pandas as pd
 
+
+app = Flask(__name__)
+
+## loaad the model
+regmodel = pickle.load(open('regmodel.pkl', 'rb'))
